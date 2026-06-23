9467c740c6b8592ea843113193a075c19b920883
Montse <mcaballerov03@gmail.com>
2026-06-22 22:08:31 -0600

    encoding

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list of
     to-do items.
     """
-    with open(filepath, 'r') as file_local:
+    with open(filepath, 'r', encoding='UTF-8') as file_local:
             todos_local = file_local.readlines()
     return todos_local
 
